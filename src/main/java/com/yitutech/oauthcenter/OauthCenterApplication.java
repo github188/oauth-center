50074fd91ec2d4b3c07ad245e5d7f9a025feaa4f
lizhijun <zhijun.li@yitu-inc.com>
2018-04-04 10:37:08 +0800

    '项目初始化'

---
+++ src/main/java/com/yitutech/oauthcenter/OauthCenterApplication.java
@@ -0,0 +1,12 @@
+package com.yitutech.oauthcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthCenterApplication.class, args);
+	}
+}
